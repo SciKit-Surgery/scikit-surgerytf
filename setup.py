f8082a78f7d5b29d88e3ad4a1a234a57ff6a075d
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-11-04 13:01:04 +0000

    Issue #1: Fix missing comma.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'h5py',
         'opencv-contrib-python==4.1.1.26',
         'tensorflow==2.0.0',
-        'tensorflow-datasets==1.3.0'
+        'tensorflow-datasets==1.3.0',
         'matplotlib=3.1.1'
     ],
 

d3579d14be1f0d51480dc5f62f266faacd1a5b77
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-04-22 13:43:33 +0100

    Issue #7: Try less data augmentation.

--- sksurgerytf/models/rgb_unet.py
+++ sksurgerytf/models/rgb_unet.py
@@ -236,24 +236,22 @@ class RGBUNet:
         train_data_gen_args = dict(rescale=1./255,
                                    horizontal_flip=True,
                                    vertical_flip=True,
-                                   fill_mode='constant',
-                                   cval=0,
-                                   rotation_range=20,
-                                   width_shift_range=[-200, 200],
-                                   height_shift_range=[-100, 100],
-                                   brightness_range=[0.75, 1.1],
-                                   zoom_range=[0.5, 1.0]
+                                   fill_mode='nearest',
+                                   rotation_range=10,
+                                   width_shift_range=0.05,
+                                   height_shift_range=0.05,
+                                   brightness_range=[0.9, 1.0],
+                                   zoom_range=[0.9, 1.0]
                                    )
 
         validate_data_gen_args = dict(rescale=1./255,
                                       horizontal_flip=True,
                                       vertical_flip=True,
-                                      fill_mode='constant',
-                                      cval=0,
-                                      rotation_range=20,
-                                      width_shift_range=[-200, 200],
-                                      height_shift_range=[-100, 100],
-                                      zoom_range=[0.5, 1.0]
+                                      fill_mode='nearest',
+                                      rotation_range=10,
+                                      width_shift_range=0.05,
+                                      height_shift_range=0.05,
+                                      zoom_range=[0.9, 1.0]
                                       )
 
         train_image_datagen = keras.preprocessing.image.ImageDataGenerator(
